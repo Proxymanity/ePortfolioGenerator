a9f2ba9187127af8ee925ec3fa10e8cc9aedc9d5
Cokers <Cokers@172.24.201.62>
2015-11-23 00:28:06 -0500

    Did Text, Hyperlink, Image

--- src/ePG/view/PageEditView.java
+++ src/ePG/view/PageEditView.java
@@ -9,8 +9,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -79,15 +81,15 @@ public class PageEditView extends BorderPane{
     }
     private void initHandlers(){
      AddHeader.setOnAction(e ->{
-        HeaderDialouge(); 
+        HeaderDialog(); 
      });
         
      AddText.setOnAction(e ->{
-         TextDialouge();
+         TextDialog();
      });
      
      AddImage.setOnAction(e -> {
-         ImageDialouge();
+         ImageDialog();
      });
      
      
@@ -98,40 +100,68 @@ public class PageEditView extends BorderPane{
         page = new Page();
     }
 
-      private void TextDialouge(){
-        Stage dialouge = new Stage();
+      private void TextDialog(){
+        Stage dialog = new Stage();
         HBox buttonBox = new HBox(); 
         Button Hyperlink = initChildButton(buttonBox, "Insert Hyperlink", "CSS",false);
-        Button changeTextFont = initChildButton(buttonBox, "Select Font", "CSS", false);
-         
-         
+        
+                
+        final ToggleGroup font = new ToggleGroup();
+        RadioButton font1 = new RadioButton("Font1");
+        font1.setToggleGroup(font);
+
+        RadioButton font2 = new RadioButton("Font 2");
+        font2.setToggleGroup(font);
+        font2.setSelected(true);
+ 
+        RadioButton font3 = new RadioButton("Font 3");
+        font3.setToggleGroup(font);
+        
+        RadioButton font4 = new RadioButton("Font 4");
+        font4.setToggleGroup(font);
+        
+        RadioButton font5 = new RadioButton("Font 5");
+        font5.setToggleGroup(font);
+        
+        HBox fontBox = new HBox();
+        fontBox.getChildren().addAll(font1,font2,font3,font4,font5);
+        
         Button Accept = new Button ("Accept");
         Accept.setOnAction(new EventHandler<ActionEvent>(){
             @Override
             public void handle(ActionEvent e){  
-                dialouge.close();
+                dialog.close();
                 }
             });
         
         VBox mainPane = new VBox();
        // mainPane.getStyleClass().add(CENTER);
         TextArea input = new TextArea();
-        mainPane.getChildren().addAll(input, Accept);
-        Scene scene = new Scene(mainPane, 250, 200);
+        
+         Hyperlink.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+             public void handle(ActionEvent e){  
+                String text = input.getText();
+                String selected = input.getSelectedText();
+                HyperlinkDialog(text,selected);
+                }
+        });
+        mainPane.getChildren().addAll(buttonBox,fontBox,input, Accept);
+        Scene scene = new Scene(mainPane, 400, 250);
        // scene.getStylesheets().add(STYLE_SHEET_UI);
-        dialouge.setTitle("Text");
-        dialouge.setScene(scene);
-        dialouge.showAndWait();
+        dialog.setTitle("Text");
+        dialog.setScene(scene);
+        dialog.showAndWait();
     }
 
     
-    private void HeaderDialouge() {
-        Stage dialouge = new Stage();
+    private void HeaderDialog() {
+        Stage dialog = new Stage();
         Button Accept = new Button ("Accept");
         Accept.setOnAction(new EventHandler<ActionEvent>(){
             @Override
             public void handle(ActionEvent e){  
-                dialouge.close();
+                dialog.close();
                 }
             });
         
@@ -141,34 +171,87 @@ public class PageEditView extends BorderPane{
         mainPane.getChildren().addAll(input, Accept);
         Scene scene = new Scene(mainPane, 250, 200);
        // scene.getStylesheets().add(STYLE_SHEET_UI);
-        dialouge.setTitle("Header");
-        dialouge.setScene(scene);
-        dialouge.showAndWait();
+        dialog.setTitle("Header");
+        dialog.setScene(scene);
+        dialog.showAndWait();
     }
 
-    private void ImageDialouge() {
-        Stage dialouge = new Stage();
+    private void ImageDialog() {
+        Stage dialog = new Stage();
         HBox SelectImage = new HBox(); 
         Button Select = initChildButton(SelectImage, "Select Image", "CSS",false);
-         
-         
+        
+        HBox captionBox = new HBox();
+        Label captionLabel = new Label("Caption : ");
+        TextField caption = new TextField();
+        captionBox.getChildren().addAll(captionLabel, caption);
+        
+        HBox HeightBox = new HBox();
+        Label heightLabel = new Label("Height :  ");
+        TextField height = new TextField();
+        HeightBox.getChildren().addAll(heightLabel, height);
+        
+        HBox WidthBox = new HBox();
+        Label widthLabel = new Label("Width :   ");
+        TextField width = new TextField();
+        WidthBox.getChildren().addAll(widthLabel, width);
+                
+        final ToggleGroup alignment = new ToggleGroup();
+        RadioButton left = new RadioButton("Left Align");
+        left.setToggleGroup(alignment);
+
+        RadioButton middle = new RadioButton("Center Align");
+        middle.setToggleGroup(alignment);
+        middle.setSelected(true);
+ 
+        RadioButton right = new RadioButton("Right Align");
+        right.setToggleGroup(alignment);
+        
+        HBox alignmentBox = new HBox();
+        alignmentBox.getChildren().addAll(left, middle, right);
+        
         Button Accept = new Button ("Accept");
         Accept.setOnAction(new EventHandler<ActionEvent>(){
             @Override
             public void handle(ActionEvent e){  
-                dialouge.close();
+                dialog.close();
                 }
             });
         
         VBox mainPane = new VBox();
        // mainPane.getStyleClass().add(CENTER);
-        TextArea input = new TextArea();
-        mainPane.getChildren().addAll(input, Accept);
-        Scene scene = new Scene(mainPane, 550, 500);
+        mainPane.getChildren().addAll(SelectImage, captionBox, HeightBox, WidthBox, alignmentBox, Accept);
+        Scene scene = new Scene(mainPane, 250, 600);
        // scene.getStylesheets().add(STYLE_SHEET_UI);
-        dialouge.setTitle("Text");
-        dialouge.setScene(scene);
-        dialouge.showAndWait();
+        dialog.setTitle("Text");
+        dialog.setScene(scene);
+        dialog.showAndWait();
     }
     
+    public void HyperlinkDialog(String text, String selected){
+       Stage dialoug = new Stage();
+        Label textArea = new Label("Your text is : " + text);
+        Label selectedText = new Label("Your selected text is : " + selected);
+
+        Label linkLabel = new Label("Enter Link :");
+        TextField link = new TextField();
+        HBox linkBox = new HBox();
+        linkBox.getChildren().addAll(linkLabel, link);
+        
+        VBox mainPane = new VBox();
+       // mainPane.getStyleClass().add(CENTER);
+        Button Accept = new Button ("Accept");
+        Accept.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent e){  
+                dialoug.close();
+                }
+            });
+        mainPane.getChildren().addAll(textArea, selectedText, linkBox, Accept);
+        Scene scene = new Scene(mainPane, 250, 600);
+       // scene.getStylesheets().add(STYLE_SHEET_UI);
+        dialoug.setTitle("HyperLink");
+        dialoug.setScene(scene);
+        dialoug.showAndWait(); 
+    }
 }
