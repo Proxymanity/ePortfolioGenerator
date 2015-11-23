4b01d296449fec9e101c4f6511133eb4ee661a9a
Cokers <Cokers@172.24.201.62>
2015-11-22 23:23:59 -0500

    Adding Dialouges for the left.

--- src/ePG/model/Page.java
+++ src/ePG/model/Page.java
@@ -5,10 +5,97 @@
  */
 package ePG.model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 /**
  *  This will hold the data for the page and have controls for editing components
  * 
  */
 public class Page {
+    ObservableList<String> Items;
+    ObservableList<String> Text;
+    ObservableList<String> Image;
+    ObservableList<String> Video;
+    ObservableList<String> SlideShow;
+    String Banner;
+    String Title;
+    String BannerImage;
+    String Footer;
+    
+    public Page(){
+        Items = FXCollections.observableArrayList();
+        Text = FXCollections.observableArrayList();
+        Image = FXCollections.observableArrayList();
+        Video = FXCollections.observableArrayList();
+        SlideShow = FXCollections.observableArrayList();
+        //Defaults
+        Banner = ("Default Banner");
+        Title = ("Title");
+        BannerImage = ("Banner Image.png");
+        Footer = ("Footer.png");       
+        Items.addAll("TEXT", "IMAGE", "VIDEO", "SLIDESHOW");
+        Text.add("SAMPLE TEXT");
+        Image.add("Image.png");
+        Video.add("Video.mp4");
+        SlideShow.add("SlideShow");
+    }        
+            
+    public VBox load() {
+        VBox display = new VBox();
+        int textCounter = 0;
+        int imageCounter = 0;
+        int videoCounter = 0;
+        int SSCounter = 0;
+        
+        
+        HBox titleBox = new HBox();
+        titleBox.getChildren().addAll(new Label("Title :") , new Label(this.Title));
+        HBox bannerBox = new HBox();
+        bannerBox.getChildren().addAll(new Label("Banner :") , new Label(this.Banner));
+        HBox bannerImageBox = new HBox();
+        bannerImageBox.getChildren().addAll(new Label("BannerImage :") , new Label(this.BannerImage));
+        HBox footerBox = new HBox();
+        footerBox.getChildren().addAll(new Label("Footer :") , new Label(this.Footer));
+        
+        display.getChildren().addAll(titleBox, bannerBox, bannerImageBox, footerBox);
+        
+        for(int i = 0; i< Items.size(); i++){
+            VBox component = new VBox();
+            Label componentName = new Label();
+            Label content = new Label();
+           if(Items.get(i).equals("TEXT")){
+               componentName.setText("TEXT");
+               content.setText(Text.get(textCounter));
+               textCounter++;
+           }
+           else if(Items.get(i).equals("IMAGE")){
+               componentName.setText("IMAGE");
+               content.setText(Image.get(imageCounter));
+               imageCounter++;
+            }
+           else if(Items.get(i).equals("VIDEO")){
+               componentName.setText("VIDEO");
+               content.setText(Video.get(videoCounter));
+               videoCounter++;
+            }
+           else if(Items.get(i).equals("SLIDESHOW")){
+               componentName.setText("SLIDESHOW");
+               content.setText(SlideShow.get(SSCounter));
+               SSCounter++;
+            }
+           
+           
+           component.getChildren().add(componentName);
+           component.getChildren().add(content);
+           display.getChildren().add(component);
+        }
+        
+        
+        return display;
+    }
     
 }
--- src/ePG/view/PageEditView.java
+++ src/ePG/view/PageEditView.java
@@ -3,14 +3,24 @@ package ePG.view;
 
 import javafx.scene.layout.VBox;
 import ePG.model.Page;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 /**
  *This UI component has the controls for editing a single page
  * Left are controls, right is the components
  */
 public class PageEditView extends BorderPane{
+    Page page;
     GeneratorView ui;
     VBox controls;
     Button changeTitle;
@@ -19,6 +29,7 @@ public class PageEditView extends BorderPane{
     Button changeColor; 
     Button changeBannerImage;
     Button changeFooter;
+    Button AddHeader;
     Button AddText;
     Button AddImage;
     Button AddVideo;
@@ -30,7 +41,9 @@ public class PageEditView extends BorderPane{
         ui = initUI;
         controls = new VBox();
         initControls();
+        initPage();
         this.setLeft(controls);
+        this.setRight(page.load());
     }
     
     private void initControls(){
@@ -39,10 +52,12 @@ public class PageEditView extends BorderPane{
         changeLayout = initChildButton(controls, "Change Layout", "CSS", false);
         changeBannerImage = initChildButton(controls, "Change Name", "CSS", false);
         changeFooter = initChildButton(controls, "Change Footer", "CSS", false);
+        AddHeader = initChildButton(controls, "Add Header", "CSS", false);
         AddText = initChildButton(controls, "Add Text", "CSS", false);
         AddImage = initChildButton(controls, "Add Image", "CSS", false);
         AddVideo = initChildButton(controls, "Add Video", "CSS", false);
         
+        initHandlers();
     }
     
      public Button initChildButton(
@@ -63,11 +78,97 @@ public class PageEditView extends BorderPane{
 	return button;
     }
     private void initHandlers(){
+     AddHeader.setOnAction(e ->{
+        HeaderDialouge(); 
+     });
         
+     AddText.setOnAction(e ->{
+         TextDialouge();
+     });
+     
+     AddImage.setOnAction(e -> {
+         ImageDialouge();
+     });
+     
+     
     }
     
-    private void TextDialouge(){
+  
+    private void initPage() {
+        page = new Page();
+    }
+
+      private void TextDialouge(){
+        Stage dialouge = new Stage();
+        HBox buttonBox = new HBox(); 
+        Button Hyperlink = initChildButton(buttonBox, "Insert Hyperlink", "CSS",false);
+        Button changeTextFont = initChildButton(buttonBox, "Select Font", "CSS", false);
+         
+         
+        Button Accept = new Button ("Accept");
+        Accept.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent e){  
+                dialouge.close();
+                }
+            });
         
+        VBox mainPane = new VBox();
+       // mainPane.getStyleClass().add(CENTER);
+        TextArea input = new TextArea();
+        mainPane.getChildren().addAll(input, Accept);
+        Scene scene = new Scene(mainPane, 250, 200);
+       // scene.getStylesheets().add(STYLE_SHEET_UI);
+        dialouge.setTitle("Text");
+        dialouge.setScene(scene);
+        dialouge.showAndWait();
+    }
+
+    
+    private void HeaderDialouge() {
+        Stage dialouge = new Stage();
+        Button Accept = new Button ("Accept");
+        Accept.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent e){  
+                dialouge.close();
+                }
+            });
+        
+        VBox mainPane = new VBox();
+       // mainPane.getStyleClass().add(CENTER);
+        TextField input = new TextField();
+        mainPane.getChildren().addAll(input, Accept);
+        Scene scene = new Scene(mainPane, 250, 200);
+       // scene.getStylesheets().add(STYLE_SHEET_UI);
+        dialouge.setTitle("Header");
+        dialouge.setScene(scene);
+        dialouge.showAndWait();
+    }
+
+    private void ImageDialouge() {
+        Stage dialouge = new Stage();
+        HBox SelectImage = new HBox(); 
+        Button Select = initChildButton(SelectImage, "Select Image", "CSS",false);
+         
+         
+        Button Accept = new Button ("Accept");
+        Accept.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent e){  
+                dialouge.close();
+                }
+            });
+        
+        VBox mainPane = new VBox();
+       // mainPane.getStyleClass().add(CENTER);
+        TextArea input = new TextArea();
+        mainPane.getChildren().addAll(input, Accept);
+        Scene scene = new Scene(mainPane, 550, 500);
+       // scene.getStylesheets().add(STYLE_SHEET_UI);
+        dialouge.setTitle("Text");
+        dialouge.setScene(scene);
+        dialouge.showAndWait();
     }
     
 }
